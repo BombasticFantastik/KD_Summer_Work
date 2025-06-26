852829dd789660f27ae06099598cebc9984904ab
BombasticFantastic <artemilusenko2@gmail.com>
2025-06-26 20:43:54 +0700

    поправил датасет, начинаем писать сетку

--- Segmentation/Dataset.py
+++ Segmentation/Dataset.py
@@ -1,5 +1,5 @@
 from torch.utils.data import Dataset
-
+from PIL import Image
 import os
 
 class Boot_Segmentation_Dataset(Dataset):
@@ -8,13 +8,13 @@ class Boot_Segmentation_Dataset(Dataset):
 
         self.all_items=[os.path.join(img_path,img) for img in os.listdir(img_path)]
         self.all_labels=[os.path.join(label_path,label) for label in os.listdir(label_path)]
-
-
-            
-
-        
-
     def __len__(self):
-        pass
-    def forward(self):
-        pass
+        return len(self.all_items)
+    def __getitem__(self,idx):
+        img=Image.open(self.all_items[idx])
+        label=Image.open(self.all_labels[idx])
+
+        return {
+            'img':img,
+            'label':label
+        }
