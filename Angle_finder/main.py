5bf79e612a40cae4cea7ec16eb67056f7e90e6df
BombasticFantastic <artemilusenko2@gmail.com>
2025-07-10 00:06:17 +0700

    добавил такую же разрезку в папке Angle_finder

--- Angle_finder/main.py
+++ Angle_finder/main.py
@@ -3,16 +3,28 @@ import cv2
 import numpy as np
 #fedora=cv2.imread(r"C:\Code\KD_PRACT\KD_Summer_Work\Angle_finder\test_fedora.png",cv2.IMREAD_UNCHANGED)#сохраняет в исходном виде
 
-img=np.ones((512,512,3),np.uint8)
+shape=(1100,762,3)
+img=np.ones(shape,np.uint8)
+weigj=shape[1]
+hight=shape[0]
+
+up=100
+left=100
+right=weigj-100
+down=hight-180
+print(up,left,right,down)
 
 
 #рисуем линии
 
 
-cv2.line(img,(0,100),(512, 100), (0, 0, 255), 5)#вверх
-cv2.line(img,(0,180),(512, 100), (0, 0, 255), 5)#вверх
-cv2.line(img,(0,100),(512, 100), (0, 0, 255), 5)#вверх
-cv2.line(img,(0,100),(512, 100), (0, 0, 255), 5)#вверх
+
+cv2.line(img,(0,up),(weigj, up), (0, 0, 255), 5)#вверх
+cv2.line(img,(left,0),(left, hight), (0, 0, 255), 5)#лево
+cv2.line(img,(right,0),(right, hight), (0, 0, 255), 5)#право
+cv2.line(img,(0,down),(weigj, down), (0, 0, 255), 5)#низ
+
+cv2.imwrite('output.jpg', img)  
 
 
 cv2.imshow('dst',img)
