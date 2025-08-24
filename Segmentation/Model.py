a84ce4ef580d9faf93bae4b4d4ca3bdd0d6666fe
BombasticFantastik <artemilusenko2@gmail.com>
2025-08-24 23:51:52 +0700

    переписал модельку, но ещё не полностью

--- Segmentation/Model.py
+++ Segmentation/Model.py
@@ -1,134 +1,53 @@
 from torch.nn import Module
 from torch import nn
 import torch
+
+class DoubleConv(Module):
+    def __init__(self,input_size,output_size):
+        super(DoubleConv,self).__init__()
+
+        self.conv_lay=nn.Sequential(
+            nn.Conv2d(input_size,output_size,kernel_size=3,padding=1),
+            nn.BatchNorm2d(output_size),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(output_size,output_size,kernel_size=3,padding=1),
+            nn.BatchNorm2d(output_size),
+            nn.ReLU(inplace=True)
+        )        
+    def forward(self,x):
+        conved_x=self.conv_lay(x)
+
+class DownConv(Module):
+    def __init__(self,input_size,output_size):
+        super(DownConv,self).__init__()
+
+        self.down_lay=DoubleConv(input_size=input_size,output_size=output_size)
+        self.down_sample=nn.MaxPool2d(2)
+
+    def forward(self,x):
+        back_conved_x=self.down_lay(x)
+        upsampled_x=self.down_sample(back_conved_x)
+        return upsampled_x
+
+class UpConv(Module):
+    def __init__(self,input_size,output_size):
+        super(UpConv,self).__init__()
+
+        self.up_samlpe=nn.ConvTranspose2d(input_size,output_size,kernel_size=2, stride=2)
+        self.double_conv=nn.DoubleConv(input_size,output_size)
+
+    def forward(self,x,skiped_x):
+        x=self.up_samlpe(x)
+        cat_x=torch.cat([x,skiped_x],dim=1)
+        return self.double_conv(cat_x)
+
+
+
+
+
+
+
+
 class Unet(Module):
     def __init__(self,input_size,hidden_dim):
         super(Unet,self).__init__()
-
-        self.ups=nn.Upsample(scale_factor=2,mode='bilinear')
-        self.sigm=nn.Sigmoid()
-
-        self.lay0=nn.Sequential(
-            nn.Conv2d(input_size,hidden_dim,3,padding=1),
-            nn.BatchNorm2d(hidden_dim),
-            nn.ReLU(),
-            nn.Conv2d(hidden_dim,hidden_dim,3,padding=1),
-            nn.BatchNorm2d(hidden_dim),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-        self.lay1=nn.Sequential(
-            nn.Conv2d(hidden_dim,hidden_dim*2,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*2),
-            nn.ReLU(),
-            nn.Conv2d(hidden_dim*2,hidden_dim*2,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*2),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-        self.lay2=nn.Sequential(
-            nn.Conv2d(hidden_dim*2,hidden_dim*4,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*4),
-            nn.ReLU(),
-            nn.Conv2d(hidden_dim*4,hidden_dim*4,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*4),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-        self.lay3=nn.Sequential(
-            nn.Conv2d(hidden_dim*4,hidden_dim*8,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*8),
-            nn.ReLU(),
-            nn.Conv2d(hidden_dim*8,hidden_dim*8,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*8),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-
-        self.lay4=nn.Sequential(
-            nn.Conv2d(hidden_dim*8,hidden_dim*16,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*16),
-            nn.ReLU(),
-            nn.Conv2d(hidden_dim*16,hidden_dim*16,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*16),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-
-        self.bottleneck=nn.Sequential(
-            nn.Conv2d(hidden_dim*16,hidden_dim*32,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*32),
-            nn.ReLU(),
-            nn.Conv2d(hidden_dim*32,hidden_dim*16,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*16),
-            nn.ReLU(),
-
-        )
-
-        self.dec_lay0=nn.Sequential(
-            nn.Conv2d(hidden_dim*32,hidden_dim*16,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*16),
-            nn.ReLU(),
-            nn.Conv2d(hidden_dim*16,hidden_dim*8,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*8),
-            nn.ReLU()
-        )
-
-        self.dec_lay1=nn.Sequential(
-            nn.Conv2d(hidden_dim*16,hidden_dim*8,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*8),
-            nn.ReLU(),
-            nn.Conv2d(hidden_dim*8,hidden_dim*4,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*4),
-            nn.ReLU()
-        )
-
-        self.dec_lay2=nn.Sequential(
-            nn.Conv2d(hidden_dim*8,hidden_dim*4,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*4),
-            nn.ReLU(),
-            nn.Conv2d(hidden_dim*4,hidden_dim*2,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*2),
-            nn.ReLU()
-        )
-
-        self.dec_lay3=nn.Sequential(
-            nn.Conv2d(hidden_dim*4,hidden_dim*2,3,padding=1),
-            nn.BatchNorm2d(hidden_dim*2),
-            nn.ReLU(),
-            nn.Conv2d(hidden_dim*2,hidden_dim,3,padding=1),
-            nn.BatchNorm2d(hidden_dim),
-            nn.ReLU()
-        )
-
-        self.dec_lay4=nn.Sequential(
-            nn.Conv2d(hidden_dim*2,hidden_dim,3,padding=1),
-            nn.BatchNorm2d(hidden_dim),
-            nn.ReLU(),
-            nn.Conv2d(hidden_dim,1,3,padding=1),
-            nn.BatchNorm2d(1),
-            nn.ReLU()
-        )
-
-    def forward(self,x):
-        #print(x.shape)
-        #encoder
-        x0=self.lay0(x)
-        x1=self.lay1(x0)
-        x2=self.lay2(x1)
-        x3=self.lay3(x2)
-        x4=self.lay4(x3)
-
-        #bottleneck
-        midl_x=self.bottleneck(x4)
-
-        #decoder
-        xdec0=self.dec_lay0(self.ups(torch.cat((midl_x,x4),dim=1)))
-        xdec1=self.dec_lay1(self.ups(torch.cat((xdec0,x3),dim=1)))
-        xdec2=self.dec_lay2(self.ups(torch.cat((xdec1,x2),dim=1)))
-        #print(xdec2.shape)
-        xdec3=self.dec_lay3(self.ups(torch.cat((xdec2,x1),dim=1)))
-        #print(xdec3.shape,x0.shape)
-        xdec4=self.dec_lay4(self.ups(torch.cat((xdec3,x0),dim=1)))
-
-        return self.sigm(xdec4)
--- Segmentation/main.py
+++ Segmentation/main.py
@@ -7,15 +7,15 @@ from torch.optim import AdamW
 from torch.nn import BCELoss
 import torch
 import torch.utils.data as data_utils
-#indices = torch.arange(500)
 
-import segmentation_models_pytorch as smp
 
-import segmen
+#import segmentation_models_pytorch as smp
+
+
 from torch.utils.data import DataLoader
 option_path=fr'/home/artemybombastic/MyGit/KD_Summer_Work/config.yml'
 device='cuda' if torch.cuda.is_available() else 'cpu'
-#device='cuda'
+device='cpu'
 print(device)
 with open(option_path,'r') as file_option:
     option=yaml.safe_load(file_option)
@@ -25,7 +25,7 @@ with open(option_path,'r') as file_option:
 dataset=Boot_Segmentation_Dataset(option['Segmentation']['img_path'],option['Segmentation']['label_path'],)
 dataloader=DataLoader(dataset=dataset,batch_size=4,drop_last=False,shuffle=True)
 assert dataset.all_items[0].split('/')[-1][:-4]==dataset.all_labels[0].split('/')[-1][:-4]
-model=smp.Unet('mobilenet_v2', encoder_weights='imagenet', classes=1, activation=None, encoder_depth=5, decoder_channels=[256, 128, 64, 16, 4])
+model=Unet(3,64).to(device)
 try:
     weights_dict=torch.load(f'/home/artemybombastic/MyGit/KD_Data/SegmData/unet_model_{device}.pth',weights_only=True)
     model.load_state_dict(weights_dict)
@@ -34,3 +34,4 @@ except:
 
 loss_fn=BCELoss()
 optimizer=AdamW(model.parameters(),lr=0.001)
+Train_model(model=model,dataloader=dataloader,loss_func=loss_fn,optimizer=optimizer,device=device)
